93034890f25d9b537486d215774dd3d30dc8024a
Wellington Ideão <wellington.ideao@gmail.com>
2021-08-29 18:45:56 -0300

    Added Tag system on posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import PostListView, PostDetailView
+from .views import PostListView, PostDetailView, PostListByTag
 
 app_name = 'blog'
 
 urlpatterns = [
     path('post/', PostListView.as_view(), name='index'),
     path('post/<slug:slug>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/tag/<slug:slug>/', PostListByTag.as_view(), name='post_list_by_tag'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from .models import Post, PublishedPost
+from .models import PublishedPost
+from taggit.models import Tag
 
 
 class PostListView(ListView):
@@ -12,6 +13,23 @@ class PostListView(ListView):
     def get_queryset(self):
         queryset = PublishedPost.objects.all()
         return queryset
+
+
+class PostListByTag(ListView):
+    template_name = 'blog/post/list_tag.html'
+    context_object_name = 'post_list'
+    paginate_by = 4
+
+    def get_queryset(self):
+        tag = get_object_or_404(Tag, slug=self.kwargs['slug'])
+        queryset = PublishedPost.objects.filter(tags__in=[tag])
+        return queryset
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        tag = get_object_or_404(Tag, slug=self.kwargs['slug'])
+        context['tag'] = tag
+        return context
 
 
 class PostDetailView(DetailView):
