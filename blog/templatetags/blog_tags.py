705635b2648a17ddfbabedb899c4ef9fe72995bc
Wellington Ideão <wellington.ideao@gmail.com>
2021-08-31 16:09:47 -0300

    Added templatetag for markdown post body text using python-markdown lib

---
+++ blog/templatetags/blog_tags.py
@@ -0,0 +1,10 @@
+from django import template
+from django.utils.safestring import mark_safe
+import markdown
+
+register = template.Library()
+
+
+@register.filter(name='markdown')
+def markdown_format(value):
+    return mark_safe(markdown.markdown(value))
